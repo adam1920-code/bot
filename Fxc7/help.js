f6689f8f3adfe0250626216a418397fd35519c5d
adam1920-code <75556463+adam1920-code@users.noreply.github.com>
2021-02-09 11:12:45 +0700

    Update help.js

--- Fxc7/help.js
+++ Fxc7/help.js
@@ -5,110 +5,26 @@ const help = (prefix, ig, name, uptime, pushname2, user, tanggal, jam) => {
 \`\`\`Follow My Instagram\`\`\`
 ${ig}
 
-\`\`\`BLOG OFFICIAL\`\`\`
-https://peoplepower107.blogspot.com
+*INI ADALAH MENU SIMPLE YEONGSILL BOT
+✘ ▬▬▬▬ ஜ۩۞۩ஜ ▬▬▬▬ ✘
+        𝙇𝙞𝙨𝙩 𝘾𝙤𝙢𝙢𝙖𝙣𝙙
+✘ ▬▬▬▬ ஜ۩۞۩ஜ ▬▬▬▬ ✘
 
-
-\`\`\`HARAP BACA NOTE DIBAWAH SEBELUM\`\`\`
-\`\`\`MENGGUNAKAN BOT\`\`\`
-
-┏━━━━━❬ *USER ${name}* ❭━━━━━━━⊱
-┃╭───────────────────────
-┃├➲ \`\`\`Hai\`\`\`👋 *${pushname2}*
-┣━━━━━━━━━━━━━━━━━
-┃├➲ \`\`\`I am\`\`\` *${name}*
-┃╰───────────────────────
-┃
-┃╭───────────────────────
-┃├➲ \`\`\`Total Pengguna:\`\`\` *${user.length} User*
-┣━━━━━━━━━━━━━━━━━
-┃├➲ \`\`\`Total Donasi:\`\`\` *0%* 🙂
-┃╰───────────────────────
-┗━━━━━━━━━━━━━━━━━━━━━━━━━┛
-
-┏━━━━━❬ *REGULATION ${name}* ❭━━━━━━━⊱
-┃
-┣⊱❥ \`\`\`AKTIF\`\`\`: ${kyun(uptime)}
-┣⊱❥ \`\`\`JAM\`\`\`: *${jam} WIB*
-┣⊱❥ \`\`\`TANGGAL\`\`\`: *${tanggal}*
-┣⊱❥ \`\`\`VERSION\`\`\`: *5.5 TERMUX*
-┃
-┣⊱❥ ❌ *SPAM*
-┣⊱❥ ❌ *CALL & VC*
-┃ \`\`\`Melanggar??\`\`\` *Banned*
-┃
-┗━━━━━━━━━━━━━━━━━━━━━━━━━┛
-
-⟣━━━━━❬ *ABOUT ${name}* ❭━━━━━━⟢
-╭────────────────────────
-├⊱❥ *${prefix}music (judul lagu)
-╰────────────────────────
-
-
-
-⟣━━━━━━❬ *OWNER ONLY* ❭━━━━━━━⟢
-╭────────────────────────
-├⊱❥ *${prefix}addprem [mentioned]*
-├⊱❥ *${prefix}removeprem [mention]*
-├⊱❥ *${prefix}setppbot*
-├⊱❥ *${prefix}bc*
-├⊱❥ *${prefix}bcgc*
-├⊱❥ *${prefix}ban*
-├⊱❥ *${prefix}unban*
-├⊱❥ *${prefix}block*
-├⊱❥ *${prefix}unblock*
-├⊱❥ *${prefix}clearall*
-├⊱❥ *${prefix}delete*
-├⊱❥ *${prefix}clone*
-├⊱❥ *${prefix}getses*
-├⊱❥ *${prefix}leave*
-╰────────────────────────
-╰────────────────────────
-
-
-⟣━━━━━━❬ *SUPPORT ${name}* ❭━━━━━━⟢
-╭────────────────────────
-│
-├➲ *ADAM OEY19*
-├➲ *KEDUA ORANG TUA*
-├➲ *MY DEVICE*
-├➲ *WHATSAPP*
-╰────────────────────────
-
-
-┏━━━━━━━━❬ *NOTE ${name}* ❭━━━━━━━━━⊱
-┃
-┣⊱❥ \`\`\`Bot ini belum selesai sepenuhnya\`\`\`
-┃ \`\`\`Masih dalam proses pengerjaan\`\`\`
-┃ \`\`\`Jadi masih jarang aktif, dan\`\`\`
-┃ \`\`\`Maaf Jika Ada Menu Yang Error\`\`\`
-┃
-┣⊱❥ \`\`\`Jika Lama Harap Ulangi Command\`\`\`
-┃
-┣⊱❥ *Gunakan Command Tanpa [ ]*
-┃
-┣⊱❥ \`\`\`Dan Jika Mengalami Error\`\`\`
-┃ \`\`\`Harap Lapor Owner Dengan Cara\`\`\`
-┃ *${prefix}report* \`\`\`apa pesan errornya\`\`\`
-┃
-┣⊱❥ \`\`\`Mau Invit Bot??lempar link,\`\`\`
-┃ \`\`\`Kalo Gamau Donasi Follow Ig\`\`\`
-┃ \`\`\`@adam_oey107\`\`\`
-┃
-┣⊱❥ \`\`\`Kalian Bisa Mempublish Quotes Kalian\`\`\`
-┃ \`\`\`Jika Minat Hubungi Aja Owner Buat\`\`\`
-┃ \`\`\`Mempublish\`\`\`
-┃ \`\`\`Quotes Kalian,\`\`\`
-┃ \`\`\`Dan Makasih Buat Temen" Yg Mau Di Public\`\`\`
-┃ \`\`\`Quotesnya:)\`\`\`
-┃
-┣⊱❥ \`\`\`Kenapa Saya Tambahkan Fitur Premium\`\`\`
-┃ \`\`\`You Know Lah Karna Fitur Tersebut Membutuhkan\`\`\`
-┃ \`\`\` Kuota Banyak Untuk Mendownload Dan Mengirimkan\`\`\`
-┃ \`\`\` Audio/ Video Yang Anda Minta\`\`\`
-┃ \`\`\` Mohon Pengertiannya🙂\`\`\`
-┃
-┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛`
+● *#sticker*
+● *_#gifstiker_*
+● *_#music [judul]_*
+● *_#ytmp3 [link yt]_*
+● *_#hidetag_*
+● *_#toimg_*
+● *_#tagall_*
+● *_#pinterest_*
+● *_#ocr_*
+● *_#wiki_*
+● *_#spamcall [62831555555]_*
+● *_#ytmp3 [link]_*
+● *_#ytmp4 [link]_*
+вσт ιηι ∂ιвυαт σℓєн:
+A.D.A.M ✔
 }
 exports.help = help
 function kyun(seconds){
