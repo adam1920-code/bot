bbffabbebcb9e3068e4a5b84ede57895a5c46243
adam1920-code <75556463+adam1920-code@users.noreply.github.com>
2021-02-09 18:36:06 +0700

    Update help.js

--- Fxc7/help.js
+++ Fxc7/help.js
@@ -27,13 +27,3 @@ ${ig}
 вσт ιηι ∂ιвυαт σℓєн:
 A.D.A.M ✔
 }
-exports.help = help
-function kyun(seconds){
-  function pad(s){
-    return (s < 10 ? '0' : '') + s;
-  }
-  var hours = Math.floor(seconds / (60*60));
-  var minutes = Math.floor(seconds % (60*60) / 60);
-  var seconds = Math.floor(seconds % 60);
-  return `*${pad(hours)} Jam ${pad(minutes)} Menit ${pad(seconds)} Detik*`
-}
